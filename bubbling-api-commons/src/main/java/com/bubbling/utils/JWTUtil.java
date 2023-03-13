0974ba4e9988ab8c899fdfa26b4bde43d5b74104
crjg <88718020+crjg-k@users.noreply.github.com>
2023-03-13 13:21:03 +0800

    jwt-utils and reflect-utils

---
+++ bubbling-api-commons/src/main/java/com/bubbling/utils/JWTUtil.java
@@ -0,0 +1,50 @@
+package com.bubbling.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Calendar;
+import java.util.Map;
+
+/**
+ * 2022-03-04 23:45:24 GMT+8
+ * JWTUtil工具类
+ * @author k
+ */
+public class JWTUtil {
+    private static String secretKey="k+5/|.|%T8C#0$b]#4";
+    /**
+     * 2022-03-04 23:40:30 GMT+8
+     * 设置密钥
+     * @param secretKey 自定义密钥
+     * @author k
+     */
+    public static void setSecretKey(String secretKey){
+        JWTUtil.secretKey =secretKey;
+    }
+    /**
+     * 2022-03-04 23:40:49 GMT+8
+     * 获取jwt-token
+     * @param map payload键值对
+     * @author k
+     */
+    public static String getJWTToken(Map<String,String> map){
+        Calendar instance = Calendar.getInstance();
+        instance.add(Calendar.HOUR, 120);   //5 days
+        JWTCreator.Builder builder = JWT.create();
+        map.forEach(builder::withClaim);
+        return builder.withExpiresAt(instance.getTime()). //指定令牌过期时间
+                sign(Algorithm.HMAC256(secretKey));
+    }
+    /**
+     * 2022-03-04 23:41:05 GMT+8
+     * 验证token合法性并返回获取token信息的类
+     * @param token jwt-token
+     * @author k
+     */
+    public static DecodedJWT verify(String token){
+        return JWT.require(Algorithm.HMAC256(secretKey)).build().verify(token);
+    }
+}
---
+++ bubbling-api-commons/src/main/java/com/bubbling/utils/ReflectUtil.java
@@ -0,0 +1,81 @@
+package com.bubbling.utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReflectUtil {
+    /**
+     * 将类的属性名和属性值放到map中
+     * 2022-03-10 14:15:18 GMT+8
+     * @param object
+     * @return
+     * @throws Exception
+     * @author k
+     */
+    public static Map<String,Object> getObjectValue(Object object) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        if (object != null) {
+            Class<?> clz = object.getClass();
+            Field[] fields = clz.getDeclaredFields();
+            for (Field field : fields) {
+                // 类型是String
+                if (field.getGenericType().toString().equals(
+                        "class java.lang.String")) { // 如果type是类类型，则前面包含"class "，后面跟类名
+                    Method m = (Method) object.getClass().getMethod(
+                            "get" + getMethodName(field.getName()));
+                    String val = (String) m.invoke(object);
+                    if (val != null) {
+                        map.put(field.getName(),val);
+                    }
+                }
+                // 类型是Integer
+                if (field.getGenericType().toString().equals(
+                        "class java.lang.Integer")) {
+                    Method m = (Method) object.getClass().getMethod(
+                            "get" + getMethodName(field.getName()));
+                    Integer val = (Integer) m.invoke(object);
+                    if (val != null) {
+                        map.put(field.getName(),val);
+                    }
+                }
+                // 类型是Double
+                if (field.getGenericType().toString().equals(
+                        "class java.lang.Double")) {
+                    Method m = (Method) object.getClass().getMethod(
+                            "get" + getMethodName(field.getName()));
+                    Double val = (Double) m.invoke(object);
+                    if (val != null) {
+                        map.put(field.getName(),val);
+                    }
+                }
+                // 类型是Date
+                if (field.getGenericType().toString().equals(
+                        "class java.util.Date")) {
+                    Method m = (Method) object.getClass().getMethod(
+                            "get" + getMethodName(field.getName()));
+                    Date val = (Date) m.invoke(object);
+                    if (val != null) {
+                        map.put(field.getName(),val);
+                    }
+                }
+            }
+        }
+        return map;
+    }
+
+    /**
+     * 把一个字符串的第一个字母大写、效率是最高的
+     * 2022-03-10 14:14:42 GMT+8
+     * @param fieldName
+     * @return
+     * @author k
+     */
+    private static String getMethodName(String fieldName){
+        byte[] items = fieldName.getBytes();
+        items[0] = (byte) ((char) items[0] - 'a' + 'A');
+        return new String(items);
+    }
+}
