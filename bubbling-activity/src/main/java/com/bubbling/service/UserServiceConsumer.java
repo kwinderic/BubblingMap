d31fee9642d1154056c58355da339b941f95060c
crjg <88718020+crjg-k@users.noreply.github.com>
2023-03-26 14:44:15 +0800

    集成redis/spring gateway/openfeign，2022-03-19 09:39:14 GMT+8

---
+++ bubbling-activity/src/main/java/com/bubbling/service/UserServiceConsumer.java
@@ -0,0 +1,13 @@
+package com.bubbling.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Component
+@FeignClient(name="User-Module-Service")
+public interface UserServiceConsumer {
+    @PostMapping("/user/login/{userPhone}/{password}/{longitude}/{latitude}")
+    String login(@PathVariable("userPhone") String userPhone, @PathVariable("password") String password, @PathVariable("longitude") String longitude, @PathVariable("latitude") String latitude);
+}
